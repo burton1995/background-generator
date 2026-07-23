70a5b80075b5d30a3ced019f72c5ab8b8d8a9f80
tony-prodkit <116398410+tony-prodkit@users.noreply.github.com>
2026-07-23 13:12:16 +0200

    added comment for littlefeature branch

--- hello_copy.py
+++ hello_copy.py
@@ -1,4 +1,5 @@
 # New Comment for Github demo
+# New Comment for littlefeature demo
 # The parameter weekday is True if it is a weekday, and the parameter vacation is True if we are on vacation. We sleep in if it is not a weekday or we're on vacation. Return True if we sleep #in.
 
 def sleep_in(weekday, vacation):
