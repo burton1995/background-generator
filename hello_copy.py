e8595811b2f8f968f60fa111694d88044535597b
tony-prodkit <116398410+tony-prodkit@users.noreply.github.com>
2026-07-23 14:17:12 +0200

    Added second try in comment

--- hello_copy.py
+++ hello_copy.py
@@ -1,5 +1,5 @@
 # New Comment for Github demo
-# New Comment for bigfeature branch demo
+# New Comment for bigfeature branch demo + second try
 # New Comment for littlefeature demo
 # The parameter weekday is True if it is a weekday, and the parameter vacation is True if we are on vacation. We sleep in if it is not a weekday or we're on vacation. Return True if we sleep #in.
 
